4edee0cbd839bee8f56ac9af4fb0c1f7ec129690
Alistair McCutcheon <alistairdmccutcheon@gmail.com>
2023-04-30 22:50:07 +1000

    Read and process data

---
+++ assignment2.py
@@ -0,0 +1,193 @@
+from __future__ import unicode_literals, print_function, division
+from io import open
+import unicodedata
+import string
+import re
+import random
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import string
+from torch.utils.data import Dataset
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print(device)
+SOS_token = 0
+EOS_token = 1
+
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Count SOS and EOS
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+
+
+# Turn a Unicode string to plain ASCII, thanks to
+# https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+def remove_successive_chars(s: str, chars_to_keep_unchanged: set[str]):
+    previous_char = None
+    new_string = []
+    for char in s:
+        if char in chars_to_keep_unchanged:
+            new_string.append(char)
+            previous_char = char
+            continue
+        if char == previous_char:
+            continue
+        new_string.append(char)
+        previous_char = char
+    return "".join(new_string)
+
+def replace(s: str, chars_to_replace: set[str], string_to_replace_with: str) -> str:
+    new_string = []
+    for char in s:
+        if char in chars_to_replace:
+            new_string.append(string_to_replace_with)
+            continue
+        new_string.append(char)
+    return "".join(new_string)
+
+def keep_chars(s, chars_to_keep: set[str]):
+    return "".join([c for c in s if c in chars_to_keep])
+
+def normalise_string(s: str):
+    s = s.lower().strip()
+    s = remove_successive_chars(s, {*list(string.ascii_lowercase), *list(string.digits)})
+    s = replace(s, {"*", "|", "\x00", ":", "_", "\x7f", "\n", "\\", "\t"}, " ")
+    s = s.replace("\"", "\'\'")
+    s = s.replace("@", " at ")
+    s = s.replace("-", " - ")
+    s = s.replace(",", " , ")
+    s = s.replace(".", " . ")
+    s = replace(s, {"&", "+"}, " and ")
+    s = replace(s, {"!", "?"}, ".")
+    s = keep_chars(s, {*list(string.ascii_lowercase), *list(string.digits), '-', ".", ",", ' '})
+    s = remove_successive_chars(s, {*list(string.ascii_lowercase), *list(string.digits)})
+    return s
+
+MAX_LENGTH = 10
+
+eng_prefixes = (
+    "i am ", "i m ",
+    "he is", "he s ",
+    "she is", "she s ",
+    "you are", "you re ",
+    "we are", "we re ",
+    "they are", "they re "
+)
+
+
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and \
+        len(p[1].split(' ')) < MAX_LENGTH and \
+        p[1].startswith(eng_prefixes)
+
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+
+def readLangs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+
+    # Read the file and split into lines
+    lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+        read().strip().split('\n')
+
+    # Split every line into pairs and normalize
+    pairs = [[normalise_string(s) for s in l.split('\t')] for l in lines]
+
+    # Reverse pairs, make Lang instances
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+
+    return input_lang, output_lang, pairs
+
+from glob import glob
+from pathlib import Path
+
+from typing import Iterable, Generator
+
+def read_ingredient_recipie(text_lines: Iterable[str]) -> Generator[tuple[str, str], None, None]:
+    for line in text_lines:
+        if not line.startswith("ingredients:"):
+            continue
+
+        ingredients = line.removeprefix("ingredients:")
+        recepie = read_next_recepie(text_lines)
+        yield ingredients, recepie
+
+def read_next_recepie(text_lines: Iterable[str]) -> str:
+    recepie_lines = []
+    for line in text_lines:
+        if line.startswith("END RECIPE"):
+            break
+        recepie_lines.append(line)
+    return "".join(recepie_lines)
+
+
+def read_ingredients_recepies(data_dir_path: str):
+    ingredients_per_recepie = []
+    recepies = []
+    for recipies_file_path in Path(data_dir_path).glob("*"):
+        with open(recipies_file_path, "r") as file:
+            for ingredients, recepie in read_ingredient_recipie(file):
+                # print(normalise_string(ingredients))
+                # print(normalise_string(recepie))
+                ingredients_per_recepie.append(normalise_string(ingredients))
+                recepies.append(normalise_string(recepie))
+    return ingredients_per_recepie, recepies
+
+
+def get_alphabet(text_lines: Iterable[str]) -> set[str]:
+    alphabet = set()
+    for text_line in text_lines:
+        alphabet.update(set(text_line)) 
+    return alphabet
+
+
+class CookingDataset(Dataset):
+    def __init__(self, data_dir_path: str) -> None:
+        super().__init__()
+
+        ingredients, recepies = read_ingredients_recepies(data_dir_path)
+        print(get_alphabet(ingredients).union(get_alphabet(recepies)))
+
+
+x = CookingDataset("/home/alistair/Code/FIT5217/data/train")
+        
+
+
+
+
